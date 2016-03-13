6c6ecf8b0dc1e9995780e9c055c4674c87cafc66
Steven Fransen <rentahippie@gmail.com>
2016-03-13 01:40:26 -0800

    Not will add all Devices to each provision profiles except iTV
    one command will add all Devices to each provision profiles except iTV as that crashes right now.
     It will also Save the Files out as the right name and will work with account with large number of Devices in license.

--- sigh/lib/sigh/download_all.rb
+++ sigh/lib/sigh/download_all.rb
@@ -1,32 +1,188 @@
+
 module Sigh
-  class DownloadAll
-    # Download all valid provisioning profiles
-    def download_all
-      UI.message "Starting login with user '#{Sigh.config[:username]}'"
-      Spaceship.login(Sigh.config[:username], nil)
-      Spaceship.select_team
-      UI.message "Successfully logged in"
+	class DownloadAll
+		# Download all valid provisioning profiles
 
-      Spaceship.provisioning_profile.all.each do |profile|
-        if profile.valid?
-          UI.message "Downloading profile '#{profile.name}'..."
-          download_profile(profile)
-        else
-          UI.important "Skipping invalid/expired profile '#{profile.name}'"
-        end
-      end
-    end
+		def download_all
+			UI.message "Starting login with user '#{Sigh.config[:username]}'"
+			Spaceship.login(Sigh.config[:username], nil)
+			Spaceship.select_team
+			UI.message "Successfully logged in"
+			FileUtils.mkdir_p(Sigh.config[:output_path])
 
-    def download_profile(profile)
-      FileUtils.mkdir_p(Sigh.config[:output_path])
-      profile_name = "#{profile.class.pretty_type}_#{profile.app.bundle_id}.mobileprovision" # default name
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "Gather All the Provison Profiles data Now"
+			t3 = Time.now
 
-      output_path = File.join(Sigh.config[:output_path], profile_name)
-      File.open(output_path, "wb") do |f|
-        f.write(profile.download)
-      end
+			File.delete('ProfileDetail.txt') if File.exist?('ProfileDetail.txt')
 
-      Manager.install_profile(output_path) unless Sigh.config[:skip_install]
-    end
-  end
+			Spaceship.provisioning_profile.all.each do |profile2|
+			t4 = Time.now
+			deltaTotal = t4 - t3
+			minutesnow = deltaTotal / 60
+			UI.message "finished in #{minutesnow} minutes Gather the info"
+			UI.message "Add All Devices to Each Provison Profiles Now"
+				if profile2.valid?
+					#p profile2
+					values = profile2.app.features
+					p values
+					File.open('ProfileDetail.txt', 'a')
+					File.write('ProfileDetail.txt', "#{profile2.devices.map(&:id).length}\t#{profile2.name}\t#{profile2.app.prefix}\t{profile2.app.prefix}.#{profile2.app.bundle_id}\t#{profile2.type}\t#{profile2.distribution_method}\t", File.size('ProfileDetail.txt'), mode: 'a')
+					File.write('ProfileDetail.txt', "#{profile2.app.features}\n", File.size('ProfileDetail.txt'), mode: 'a')
+
+					p "#{profile2.devices.map(&:id).length} #{profile2.name}"
+					UI.message "Update profile '#{profile2.name}'"
+					update_profile(profile2)
+				else
+					UI.important "Skipping invalid/expired profile '#{profile2.name}'"
+				end
+			end
+			t5 = Time.now
+			deltaTotal2 = t5 - t3
+			minutesnow2 = deltaTotal2 / 60
+			UI.message "finished in #{minutesnow2} minutes to add all Devices to each Provison Profile"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "Download Each Provison Profiles Now"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+			UI.message "#####################"
+
+			t6 = Time.now
+			UI.message "Gather All the Provison Profiles data the Second Time Now"
+			Spaceship.provisioning_profile.all.each do |profile|
+			t7 = Time.now
+			deltaTotal3 = t7 - t6
+			minutesnow3 = deltaTotal3 / 60
+			UI.message "finished in #{minutesnow3} minutes to Gather the info again"
+				if profile.valid?
+					UI.message "Downloading profile '#{profile.name}'..."
+					download_profile(profile)
+				else
+					UI.important "Skipping invalid/expired profile '#{profile.name}'"
+				end
+			end
+			t8 = Time.now
+			deltaTotal6 = t8 - t3
+			minutesnow6 = deltaTotal6 / 60
+			print "============================ finished in #{minutesnow6} minutes \n"
+		end
+
+		def update_profile(profile)
+			FileUtils.mkdir_p(Sigh.config[:output_path])
+			#profile_name = "#{profile.class.pretty_type}_#{profile.app.bundle_id}.mobileprovision" # default name
+			profile_name = "#{profile.name}.mobileprovision"
+
+			# Push the changes back to the Apple Developer Portal
+			UI.message "profile Name is " + profile_name
+
+			UI.message "#####################"
+
+			UI.message "profile.name is " + profile.name
+			UI.message "type is " + profile.type
+			UI.message "distribution_method is " + profile.distribution_method
+			UI.message "profile.id is " + profile.id
+			UI.message "#####################"
+
+			if profile.type.include? "tvOS"
+				UI.message "#####################"
+				UI.message "This is iTV Profiles "
+				UI.message "#####################"
+				#p	profile
+				goodname = profile_name.gsub(" ", "_")
+				UI.message "Don't add Devices to this Provision Profile " + goodname
+			else
+				if profile.distribution_method.include? "store"
+					#p	profile
+					goodname = profile_name.gsub(" ", "_")
+					UI.message "Don't add Devices to this Provision Profile " + goodname
+				else
+					#UI.message profile
+					UI.important "Updating Devices in this profile to include all devices"
+					profile.devices = Spaceship.device.all_for_profile_type(profile.type)
+
+					UI.message "Saved Back to Apple"
+					profile.update!
+				 end
+			 end
+		end
+
+		def download_profile(profile)
+			FileUtils.mkdir_p(Sigh.config[:output_path])
+			profile_name = "#{profile.name}.mobileprovision"
+
+			UI.message "#####################"
+			UI.message "profile.name is " + profile.name
+			UI.message "type is " + profile.type
+			UI.message "distribution_method is " + profile.distribution_method
+			UI.message "profile.id is " + profile.id
+			UI.message "#{profile.devices.map(&:id).length} #{profile.name}"
+			UI.message "#####################"
+
+			goodname = profile_name.gsub(" ", "_")
+			UI.message "Don't add Devices to this Provision Profile " + goodname + " just Downlod"
+			output_path = File.join(Sigh.config[:output_path], goodname)
+			File.delete(output_path) if File.exist?(output_path)
+			File.open(output_path, "wb") do |f|
+				f.write(profile.download)
+			end
+
+# 			if profile.type.include? "tvOS"
+# 				UI.message "#####################"
+# 				UI.message "This is iTV Profiles only download this"
+# 				UI.message "#####################"
+# 				#p	profile
+# 				p "#{profile.devices.map(&:id).length} #{profile.name}"
+# 				goodname = profile_name.gsub(" ", "_")
+# 				UI.message "Don't add Devices to this Provision Profile " + goodname + " just Downlod"
+# 				output_path = File.join(Sigh.config[:output_path], goodname)
+# 				File.open(output_path, "wb") do |f|
+# 					f.write(profile.download)
+# 				end
+# 			else
+# 				if profile.distribution_method.include? "store"
+# 					#p	profile
+# 					goodname = profile_name.gsub(" ", "_")
+# 					UI.message "Don't add Devices to this Provision Profile " + goodname + " just Downlod"
+# 					output_path = File.join(Sigh.config[:output_path], goodname)
+# 					File.open(output_path, "wb") do |f|
+# 						f.write(profile.download)
+# 					end
+# 				else
+# 					#UI.message profile
+# 					UI.important "Download the new profile with include all devices"
+# 					profile.devices = Spaceship.device.all_for_profile_type(profile.type)
+# 					p "#{profile.devices.map(&:id).length} #{profile.name}"
+#
+# 					UI.message "Saved Back to Apple"
+# 					goodname = profile_name.gsub(" ", "_")
+# 					UI.message "Download this Provision Profile " + goodname + " with all Devices now"
+# 					output_path = File.join(Sigh.config[:output_path], goodname)
+# 					File.open(output_path, "wb") do |f|
+# 						f.write(profile.download)
+# 					end
+# 				 end
+# 			end
+
+#			Manager.install_profile(output_path) unless Sigh.config[:skip_install]
+		end
+
+		def download_profile2(profile)
+			FileUtils.mkdir_p(Sigh.config[:output_path])
+			profile_name = "#{profile.class.pretty_type}_#{profile.app.bundle_id}.mobileprovision" # default name
+
+			output_path = File.join(Sigh.config[:output_path], profile_name)
+			File.open(output_path, "wb") do |f|
+				f.write(profile.download)
+			end
+
+			Manager.install_profile(output_path) unless Sigh.config[:skip_install]
+		end
+	end
 end
